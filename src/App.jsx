b84871e056fc200d17992cef89fc9876f1ba5d81
Eman <jutteman19@gmail.com>
2026-08-12 11:22:33 +0500

    Fix gitignore and rebrand to EventBooking

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-import Navbar from "./components/Navbar";
-import ProtectedRoute from "./components/ProtectedRoute";
+import Login from "./pages/Login";
 import Home from "./pages/Home";
 import EventDetail from "./pages/EventDetail";
-import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Admin from "./pages/Admin";
-
+import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
 export default function App() {
   return (
     <BrowserRouter>
